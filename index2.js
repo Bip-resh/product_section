de85dd02e276595239082e97eea4fff2197909e3
Bipresh <142672180+Bip-resh@users.noreply.github.com>
2026-06-19 14:52:30 +0600

    Refactor product quantity and price update logic

--- index2.js
+++ index2.js
@@ -1,11 +1,17 @@
 const productNumbers = document.querySelector(".number_of_products");
 const addNumber = document.querySelector(".addProduct");
 const subtractNumber = document.querySelector(".subtractProduct");
+const priceOfProduct = document.querySelector("#price-money");
+const price = Number(priceOfProduct.textContent.replace("$", ""));
+
 
 addNumber.addEventListener("click", () => {
 
     let currentValue = Number(productNumbers.textContent);
-    productNumbers.textContent = currentValue + 1;
+    let value = currentValue + 1;
+    productNumbers.textContent = value;
+    
+    addPrice(value);
     
 });
 
@@ -13,9 +19,17 @@ addNumber.addEventListener("click", () => {
 subtractNumber.addEventListener("click", () => {
     
     let currentValue = Number(productNumbers.textContent);
-    currentValue > 0 ? productNumbers.textContent = currentValue - 1 : productNumbers.textContent = 0;
+    let value = currentValue - 1;
+    currentValue > 0 ? productNumbers.textContent = value : productNumbers.textContent = 0;
    
-
+    addPrice(value);
 });
 
 
+let addPrice = (value) => {
+    
+    let currentPrice;
+    value > 0 ? currentPrice = price * value : currentPrice = 0;
+
+    priceOfProduct.textContent = `$${currentPrice}`
+}
